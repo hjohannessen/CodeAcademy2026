65c4b2813b9dc549fa59e9f98e53e8da2f9a5fcb
Herman Johannessen <herman.anker@soprasteria.com>
2026-04-29 17:42:56 +0200

    basic setup

--- 2-EventDriven/dotnet-consumer/Producer/Program.cs
+++ 2-EventDriven/dotnet-consumer/Producer/Program.cs
@@ -1,9 +1,10 @@
-using CodeAcademy.DotnetConsumer.Common.Config;
-using RabbitMQ.Client;
 using System.Text;
 using System.Text.Json;
+using CodeAcademy.DotnetConsumer.Common.Config;
+using RabbitMQ.Client;
 
 Console.WriteLine("Producer starting...");
+
 // Establish connection to RabbitMQ
 using var connection = await ConnectionHelper.ConnectAsync();
 Console.WriteLine("Connected to RabbitMQ");
@@ -13,3 +14,36 @@ Console.WriteLine("Connected to RabbitMQ");
 // - Create a channel
 // - Declare a queue
 // - Publish a message to the queue (you can use a simple JSON string as the message body)
+
+
+// Create a channel and declare the queue
+using var channel = await connection.CreateChannelAsync();
+await channel.QueueDeclareAsync(
+    queue: "idem-events",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+// Publish messages to the queue with for loop to simulate multiple events
+for (int i = 0; i < 10; i++)
+{
+    var message = $"Idems Event {i + 1} at {DateTime.Now}";
+
+    var messageBody = JsonSerializer.Serialize(message);
+    var body = Encoding.UTF8.GetBytes(messageBody);
+
+    await channel.BasicPublishAsync(
+        exchange: string.Empty,
+        routingKey: "idem-events",
+        mandatory: true,
+        basicProperties: new BasicProperties { Persistent = true },
+        body: body
+    );
+    Console.WriteLine($"Published event: {message}");
+
+    await Task.Delay(2000);
+}
+
+Console.WriteLine("Producer finished.");
